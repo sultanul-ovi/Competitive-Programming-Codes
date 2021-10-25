a7d0cf78f184bc66439b36f69d17cd2be39c61a7
Md. Sultanul Islam Ovi <sultanulislam@iut-dhaka.edu>
2021-10-25 20:37:03 +0600

    1018

--- URI/tempCodeRunnerFile.c
+++ URI/tempCodeRunnerFile.c
@@ -2,18 +2,29 @@
 
 int main(){
 
+    int n=0,n100=0,n50=0,n20=0,n10=0,n5=0,n2=0,n1=0;
 
-    char name[20];
+    scanf("%d", &n);
 
-    float profit,salary;
+    n100 = n/100;
+    n50 = (n%100)/50;
+    n20 = ((n%100)%50)/20;
+    n10 = (((n%100)%50)%20)/10;
+    n5 = ((((n%100)%50)%20)%10)/5;
+    n2 = (((((n%100)%50)%20)%10)%5)/2;
+    n1 = ((((((n%100)%50)%20)%10)%5)%2)/1;
 
-    scanf("%s", name);
-    scanf("%f", &salary);
-    scanf("%f", &profit);
 
-    salary = (salary+.15*profit);
+    
 
-    printf("TOTAL = R$ %.2f\n", salary);
+    printf("%d nota(s) de R$ 100,00\n", n100);
+    printf("%d nota(s) de R$ 50,00\n", n50);
+    printf("%d nota(s) de R$ 20,00\n", n20);
+    printf("%d nota(s) de R$ 10,00\n", n10);
+    printf("%d nota(s) de R$ 5,00\n", n5);
+    printf("%d nota(s) de R$ 2,00\n", n2);
+    printf("%d nota(s) de R$ 1,00\n", n1);
+
 
     return 0;
 }
